ad877377054b6ad0375bd2a4d7b8b873a232c304
Rashidjuma198 <rashidjumaoti@gmail.com>
2025-06-04 23:09:25 +0300

    I have implemented multiplication function

--- index.py
+++ index.py
@@ -4,8 +4,8 @@ def add(a, b):
 
 # def subtract(a, b):
 #     return a - b
-# def multiply(a, b):
-#     return a * b
+def multiply(a, b):
+    return a * b
 
 # def divide(a, b):
 #     if b == 0:
@@ -16,10 +16,10 @@ def add(a, b):
 
 add_result = add(5, 3)
 # subtract_result = subtract(10, 4)
-# multiply_result = multiply(6, 7)
+multiply_result = multiply(6, 7)
 # divide_result = divide(20, 5)
 
 print(f"Addition: {add_result}")
 # print(f"Subtraction: {subtract_result}")    
-# print(f"Multiplication: {multiply_result}")
+print(f"Multiplication: {multiply_result}")
 # print(f"Division: {divide_result}")
